250ff6e8c3cf30fff64ceb62d17cd09017a3f079
mingeiwang <875345959@qq.com>
2018-03-09 09:25:54 +0800

    更新内容

--- src/main/java/com/commons/filter/AuthIntercepter.java
+++ src/main/java/com/commons/filter/AuthIntercepter.java
@@ -12,19 +12,8 @@ public class AuthIntercepter extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		String url = request.getRequestURI();
-		try {
-			return deal(request, response, url);
-		} catch (Exception e) {
-			response.sendRedirect("error");
-			return false;
-		}
-		
-	}
-
-	private boolean deal(HttpServletRequest request, HttpServletResponse response, String url) throws IOException {
-		String userName = request.getParameter("userName");
-		return "userName147852369".equals(userName);
+		//your code
+		return true;
 	}
 	
 }
--- src/main/java/com/thread/demo/controller/TestController.java
+++ src/main/java/com/thread/demo/controller/TestController.java
@@ -1,10 +1,36 @@
 package com.thread.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.commons.beans.ResultBean;
+import com.thread.demo.entity.BaseCount;
+import com.thread.demo.service.TestDemoService;
 
 @Controller
 @RequestMapping(value = "/test")
 public class TestController {
 
+	@Autowired
+	private TestDemoService demoService;
+	
+	@RequestMapping(value = "/test1")
+	@ResponseBody
+	public ResultBean<BaseCount> addCount(Long id){
+		return new ResultBean<>(demoService.addCount(id));
+	}
+	
+	@RequestMapping(value = "/test2")
+	@ResponseBody
+	public ResultBean<BaseCount> addCountLock(Long id){
+		return new ResultBean<>(demoService.addCountLock(id));
+	}
+	
+	@RequestMapping(value = "/test3")
+	@ResponseBody
+	public ResultBean<BaseCount> addCountSyn(Long id){
+		return new ResultBean<>(demoService.addCountSyn(id));
+	}
 }
